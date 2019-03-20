c7369ae9ae3a1c71e97f3f56f025354184798a5c
Dimitar Tasev <dimtasev@gmail.com>
2019-03-20 12:01:45 +0000

    Show if the PR is review approved, move Merged to the back

--- get_gh_prs.js
+++ get_gh_prs.js
@@ -12,8 +12,16 @@
   'use strict';
   const YOUR_GITHUB_API_KEY = "";
   const container = document.getElementsByClassName("main-container")[0];
+  const GITHUB_PR_API = "https://api.github.com/repos/mantidproject/mantid/pulls/";
+
   is_document_ready();
+
+  /**
+   * Starts the interval if Dynalist has finished loading, signified by the
+   * main loader on the screen becoming invisible.
+   */
   function is_document_ready() {
+
     var visibleLoader = null;
     for (const loader of container.getElementsByClassName("loader")) {
       if (loader.visible) {
@@ -29,7 +37,6 @@
       setTimeout(is_document_ready, 1000);
     }
   }
-  const GITHUB_PR_API = "https://api.github.com/repos/mantidproject/mantid/pulls/";
   function get_all_prs() {
     for (const tag of document.getElementsByClassName("node-tag")) {
       // yikes, but what can you do, the structure is weird, and we want to avoid updating checked tags
@@ -49,7 +56,8 @@
             const pr_id = link.href.slice(last_backslash + 1);
             // make a PR data object to add more stuff to it, e.g. merged or not
             const pr_custom_data = { id: pr_id };
-            get(GITHUB_PR_API + pr_id, tag, pr_custom_data, get_pr_statuses);
+            // get the reviews, by calling into GET with the REVIEWS url
+            get(GITHUB_PR_API + pr_id + "/reviews", tag, pr_custom_data, get_pr_reviews);
           }
         }
 
@@ -57,11 +65,24 @@
     }
   }
 
+  function get_pr_reviews(link_tag, pr_custom_data, review_data) {
+    if (review_data.length > 0) {
+      pr_custom_data.approved = review_data[0].state === "APPROVED";
+      pr_custom_data.review_user = review_data[0].user.login;
+    } else {
+      pr_custom_data.approved = false;
+      pr_custom_data.review_user = "";
+    }
+    // get the full PR data, by calling into GET with the single PR data URL
+    get(GITHUB_PR_API + pr_custom_data.id, link_tag, pr_custom_data, get_pr_statuses);
+  }
+
   function get_pr_statuses(link_tag, pr_custom_data, pr_data) {
     pr_custom_data.merged = pr_data.merged;
-    // just get the statuses data, and callback into updating the PR tags
-    get(pr_data._links.statuses.href, link_tag, pr_custom_data, update_pr_tags);
+    // get the PR build status data, by calling into GET with the statuses details URL
+    get(pr_data.statuses_url, link_tag, pr_custom_data, update_pr_tags);
   }
+
 
   function update_pr_tags(link_tag, pr_custom_data, status_data) {
     // tag status already exists, update it
@@ -72,15 +93,24 @@
     } else {
       var a = document.createElement("a");
       var status = document.createElement("a");
+      var review = document.createElement("a");
 
       a.className = "node-link fffff";
-      status.className = "node-link fffff";
 
+      status.className = "node-link fffff";
       status.style.backgroundColor = "mediumpurple";
       status.style.color = "black";
       status.style.display = "none";
+
+      review.style.display = "none";
+      review.className = "node-link fffff";
+      review.style.backgroundColor = "chartreuse";
+      review.style.color = "black";
+
       link_tag.parentElement.appendChild(a);
-      link_tag.parentElement.insertBefore(status, link_tag.parentElement.children[0]);
+      link_tag.parentElement.appendChild(review);
+      link_tag.parentElement.appendChild(status);
+      // link_tag.parentElement.insertBefore(status, link_tag.parentElement.children[0]);
     }
 
     switch (status_data[0].state) {
@@ -99,8 +129,13 @@
     a.textContent += " " + status_data[0].context;
 
     if (pr_custom_data.merged) {
-      status.textContent = "merged";
+      status.textContent = "Merged";
       status.style.display = "inline-block";
+    }
+
+    if (pr_custom_data.approved) {
+      review.textContent = "✓ Approved by " + pr_custom_data.review_user;
+      review.style.display = "inline-block";
     }
   }
   function get(url, link_tag, pr_custom_data, callback) {
